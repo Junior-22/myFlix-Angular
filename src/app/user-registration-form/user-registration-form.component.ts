9505b40f19f820accb7a6531bc39a98422a26ada
Junior-22 <juniorfrancis2608@gmail.com>
2022-08-06 21:22:16 +0200

    catch errors

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -29,16 +29,17 @@ export class UserRegistrationFormComponent implements OnInit {
     this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
       // Logic for a successful user registration goes here
       this.dialogRef.close();  // This will close the modal on success
-      console.log(result);
+      console.log('line 32 result:', result);
       this.snackBar.open("successfully registered", "OK", {
         duration: 2000
       });
     }, (result) => {
-      console.log(result);
+      console.log('line 37 result: ', result);
       this.snackBar.open(result, "OK", {
         duration: 2000
       });
     });
+    ;
   }
 
 }
