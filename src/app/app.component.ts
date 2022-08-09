e742b80950d688ef16119c6e90781950ac6c3034
Junior-22 <juniorfrancis2608@gmail.com>
2022-08-09 12:01:57 +0200

    transferred code to welcome page component for routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MatDialog } from '@angular/material/dialog';
+// import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+// import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+// import { MatDialog } from '@angular/material/dialog';
+// import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -11,21 +12,27 @@ import { MatDialog } from '@angular/material/dialog';
 export class AppComponent {
   title = 'myFlix-angular-client';
 
-  constructor(public dialog: MatDialog) { }
+  // constructor(public dialog: MatDialog) { }
 
-  // function that will open the dialog when the signup button is clicked
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      // assign the dialog a width
-      width: "480px"
-    });
-  }
+  // // function that will open the dialog when the signup button is clicked
+  // openUserRegistrationDialog(): void {
+  //   this.dialog.open(UserRegistrationFormComponent, {
+  //     // assign the dialog a width
+  //     width: "480px"
+  //   });
+  // }
 
-  // function that will open the dialog when the login button is clicked
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      width: "280px"
-    });
-  }
+  // // function that will open the dialog when the login button is clicked
+  // openUserLoginDialog(): void {
+  //   this.dialog.open(UserLoginFormComponent, {
+  //     width: "280px"
+  //   });
+  // }
+
+  // openMoviesDialog(): void {
+  //   this.dialog.open(MovieCardComponent, {
+  //     width: "500px"
+  //   });
+  // }
 
 }
