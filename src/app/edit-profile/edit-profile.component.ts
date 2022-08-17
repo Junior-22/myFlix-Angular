c28bf747e68c0d80bc98cf16c6f6a8eb9d81bf9e
Junior-22 <juniorfrancis2608@gmail.com>
2022-08-17 19:46:16 +0200

    implement code to edit profile

---
+++ src/app/edit-profile/edit-profile.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-edit-profile',
+  templateUrl: './edit-profile.component.html',
+  styleUrls: ['./edit-profile.component.scss']
+})
+export class EditProfileComponent implements OnInit {
+
+  @Input() userData: any = {};
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<EditProfileComponent>,
+    public snackbar: MatSnackBar,
+    public router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  editUser(): void {
+    console.log(this.userData);
+    this.fetchApiData.editUser(this.userData).subscribe((result) => {
+      this.dialogRef.close();
+      console.log(result);
+      this.snackbar.open("Profile updated", "OK", {
+        duration: 2000
+      });
+      // let user sign in after update
+      if (this.userData.Username || this.userData.Password) {
+        localStorage.clear();
+        this.router.navigate(["welcome"]);
+        this.snackbar.open("Please login", "OK", {
+          duration: 2000
+        });
+      }
+    })
+  }
+
+}
