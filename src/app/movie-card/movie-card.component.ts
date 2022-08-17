92e1af63471d61ad0ee9398638915b22b359e6ca
Junior-22 <juniorfrancis2608@gmail.com>
2022-08-17 13:18:10 +0200

    add director, genre & description dialog functions

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 
+import { DirectorViewComponent } from '../director-view/director-view.component';
+import { GenreViewComponent } from '../genre-view/genre-view.component';
+import { DescriptionViewComponent } from '../description-view/description-view.component';
+
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
@@ -10,7 +17,11 @@ export class MovieCardComponent implements OnInit {
 
   movies: any[] = [];
 
-  constructor(public fetchApiData: FetchApiDataService) { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
     this.getMovies();
@@ -21,6 +32,35 @@ export class MovieCardComponent implements OnInit {
       this.movies = resp;
       console.log(this.movies);
       return this.movies;
+    });
+  }
+
+  openDirectorDialog(name: string): void {
+    this.dialog.open(DirectorViewComponent, {
+      data: {
+        Name: name
+      },
+      // Assign dialog width
+      width: '500px'
+    });
+  }
+
+  openGenreDialog(name: string): void {
+    this.dialog.open(GenreViewComponent, {
+      data: {
+        Name: name
+      },
+      width: '500px'
+    });
+  }
+
+  openDescriptionDialog(title: string, description: string): void {
+    this.dialog.open(DescriptionViewComponent, {
+      data: {
+        Title: title,
+        Description: description
+      },
+      width: '500px'
     });
   }
 
